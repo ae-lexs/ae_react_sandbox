90d51ea99e68b5ea40e97ecf38115eeca6e90e11
ae-lexs <brian.alexis.nava.arreola@gmail.com>
2022-02-05 19:49:19 -0600

    feat: Add styles to the counter.

--- src/container/counter/index.tsx
+++ src/container/counter/index.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Box, Button, TextField, Typography } from '@mui/material';
 
 import { useStoreDispatch, useStoreSelector } from '../../store/hooks';
 import {
@@ -14,34 +15,56 @@ export default function Counter() {
   const dispatch = useStoreDispatch();
 
   return (
-    <div>
-      <div>
-        <button type="button" onClick={() => dispatch(increment())}>
-          Increment
-        </button>
-        <span>{counterValue}</span>
-        <button type="button" onClick={() => dispatch(decrement())}>
-          Decrement
-        </button>
+    <Box
+      sx={{
+        bgcolor: 'background.paper',
+        boxShadow: 1,
+        borderRadius: 1,
+        display: 'flex',
+        flexDirection: 'column',
+        p: 2,
+        minWidth: 300,
+      }}
+    >
+      <Box
+        sx={{
+          display: 'flex',
+        }}
+      >
+        <Box sx={{ color: 'text.secondary' }}>
+          <Typography>Counter Value</Typography>
+        </Box>
+        <Box sx={{ color: 'text.primary', fontSize: 34, fontWeight: 'medium' }}>
+          <Typography>{counterValue}</Typography>
+        </Box>
+      </Box>
+      <Button variant="outlined" onClick={() => dispatch(increment())}>
+        Increment
+      </Button>
 
-        <input
-          type="number"
-          value={incrementAmount}
-          onChange={(event) => setIncrementAmount(Number(event.target.value))}
-        />
-        <button
-          type="button"
-          onClick={() =>
-            dispatch(
-              incrementByAmount({
-                value: incrementAmount,
-              }),
-            )
-          }
-        >
-          Increment By Amount
-        </button>
-      </div>
-    </div>
+      <Button variant="outlined" onClick={() => dispatch(decrement())}>
+        Decrement
+      </Button>
+
+      <TextField
+        variant="outlined"
+        type="number"
+        value={incrementAmount}
+        onChange={(event) => setIncrementAmount(Number(event.target.value))}
+      />
+
+      <Button
+        variant="outlined"
+        onClick={() =>
+          dispatch(
+            incrementByAmount({
+              value: incrementAmount,
+            }),
+          )
+        }
+      >
+        Increment By Amount
+      </Button>
+    </Box>
   );
 }
