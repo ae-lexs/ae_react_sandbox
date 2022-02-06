a1752067c3379d1134fbb9b91205fc7beaf3fc02
ae-lexs <brian.alexis.nava.arreola@gmail.com>
2022-02-05 19:05:10 -0600

    feat: Set up material UI.

--- src/container/home/index.tsx
+++ src/container/home/index.tsx
@@ -1,3 +1,9 @@
+import Typography from '@mui/material/Typography';
+
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <div>
+      <Typography variant="h1">Home</Typography>
+    </div>
+  );
 }
